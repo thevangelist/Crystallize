eaee0c336a59554080193ada4a0538a48ee9aa60
Esa Juhana Lahikainen <info@esajuhana.com>
2015-10-03 03:47:32 +0300

    Replace complex validations with simple ones

--- app/public/custom.js
+++ app/public/custom.js
@@ -1,49 +1,8 @@
 $(document).ready(function(){
-    $('#crystallized').validate({
-        rules: {
-            company: {
-                required: true
-            },
-            email: {
-                required: true,
-                email: true
-            },
-            category_cards_green: {
-                required: true
-            },
-            category_cards_red: {
-                required: true
-            },
-            topaasia_green: {
-                required: true
-            },
-            improvement_green: {
-                required: true
-            },
-            lead_green: {
-                required: true
-            },
-            topaasia_red: {
-                required: true
-            },
-            improvement_red: {
-                required: true
-            },
-            lead_red: {
-                required: true
-            },
-            rating: {
-                required: true
-            },
-            last_used: {
-                required: true
-            }
-        },
-        highlight: function (label) {
-            $(label).closest('.control-group').removeClass('success').addClass('error');
-        },
-        success: function (label) {
-            label.text('OK').addClass('valid').closest('.control-group').removeClass('error').addClass('success');
-        }
+    $('#crystallized').validate();
+        $('input, textarea').each(function () {
+        $(this).rules('add', {
+            required: true
+        });
     });
 });
