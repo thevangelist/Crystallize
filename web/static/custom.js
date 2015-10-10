efcbb70e62372894984e28bb8323ea8098d4c548
Esa Juhana Lahikainen <esa.lahikainen@descom.fi>
2015-10-10 22:04:01 +0300

    Fix jquery email validation bug, closes #9

--- web/static/custom.js
+++ web/static/custom.js
@@ -1,8 +1,28 @@
 $(document).ready(function(){
-    $('#crystallized').validate();
-        $('input, textarea').each(function () {
-        $(this).rules('add', {
-            required: true
-        });
+  $.validator.addMethod( //override email with django email validator regex - fringe cases: "user@admin.state.in..us" or "name@website.a"
+      'strictemail',
+      function(value, element){
+          return this.optional(element) || /(^[-!#$%&'*+/=?^_`{}|~0-9A-Z]+(\.[-!#$%&'*+/=?^_`{}|~0-9A-Z]+)*|^"([\001-\010\013\014\016-\037!#-\[\]-\177]|\\[\001-\011\013\014\016-\177])*")@((?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)$)|\[(25[0-5]|2[0-4]\d|[0-1]?\d?\d)(\.(25[0-5]|2[0-4]\d|[0-1]?\d?\d)){3}\]$/i.test(value);
+      },
+      "Sy&ouml;t&auml; toimiva s&auml;hk&ouml;postiosoite"
+  );
+
+  $('#crystallized').validate();
+
+  $('input[type="text"],input[type="radio"], input[type="checkbox"], textarea').each(function () {
+    $(this).rules('add', {
+      required: true
     });
+    $('input[type="email"]').each(function () {
+      $(this).rules('add', {
+        required:  {
+          depends:function(){
+            $(this).val($.trim($(this).val()));
+            return true;
+          }
+        },
+        strictemail: true
+      });
+    });
+  });
 });
