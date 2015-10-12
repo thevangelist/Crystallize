e55764fb065814bbd1ca7ad6c553bb9dc6376d5b
Matias Keveri <keveri@users.noreply.github.com>
2015-10-12 21:37:00 +0300

    Use lazy text instead of converting to strings.

--- src/Formalize/Pdf.hs
+++ src/Formalize/Pdf.hs
@@ -2,19 +2,18 @@ module Formalize.Pdf
     ( createPDF
     ) where
 
-import Data.String.Conversions as C
 import Formalize.Html
 import Formalize.Types
 import System.Process
 import System.IO.Temp
-import System.IO as IO
-import qualified Data.ByteString as BS
+import System.IO (hClose, Handle)
+import qualified Data.ByteString   as BS
+import qualified Data.Text.Lazy    as LT
+import qualified Data.Text.Lazy.IO as IO
 
 -- Create PDF file from form data.
 createPDF :: FormData -> IO PDF
-createPDF fd = do
-    htmlText <- pdfHtml fd
-    html2PDF $ C.cs htmlText
+createPDF fd = pdfHtml fd >>= html2PDF
 
 -- Options for wkhtmltopdf tool.
 pdfOptions :: [String]
@@ -22,12 +21,11 @@ pdfOptions = [ "--quiet"
              , "--user-style-sheet", "web/static/pdf.css"
              ]
 
--- TODO: dont use string?
 -- Use wkhtmltopdf tool for converting HTML to PDF.
-html2PDF :: String -> IO PDF
+html2PDF :: LT.Text -> IO PDF
 html2PDF html = withSystemTempFile "output.pdf" (html2PDF' html)
   where
-    html2PDF' :: String -> FilePath -> Handle -> IO PDF
+    html2PDF' :: LT.Text -> FilePath -> Handle -> IO PDF
     html2PDF' html' tempPDFFile tempPDFHandle = do
       hClose tempPDFHandle
       withSystemTempFile "input.html" $ \tempHtmlFile tempHtmlHandle -> do
