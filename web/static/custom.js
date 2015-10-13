5b35e1c1885bbca77447541ec5fbc93fdc941bfb
Esa Juhana Lahikainen <info@esajuhana.com>
2015-10-13 17:39:56 +0300

    Add basic backend flash msg with dismiss script (fix some js indents as well). UI for #4

--- web/static/custom.js
+++ web/static/custom.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
+
   $.validator.addMethod( //override email with django email validator regex - fringe cases: "user@admin.state.in..us" or "name@website.a"
-      'strictemail',
-      function(value, element){
-          return this.optional(element) || /(^[-!#$%&'*+/=?^_`{}|~0-9A-Z]+(\.[-!#$%&'*+/=?^_`{}|~0-9A-Z]+)*|^"([\001-\010\013\014\016-\037!#-\[\]-\177]|\\[\001-\011\013\014\016-\177])*")@((?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)$)|\[(25[0-5]|2[0-4]\d|[0-1]?\d?\d)(\.(25[0-5]|2[0-4]\d|[0-1]?\d?\d)){3}\]$/i.test(value);
-      },
-      "Sy&ouml;t&auml; toimiva s&auml;hk&ouml;postiosoite"
+    'strictemail',
+    function(value, element){
+      return this.optional(element) || /(^[-!#$%&'*+/=?^_`{}|~0-9A-Z]+(\.[-!#$%&'*+/=?^_`{}|~0-9A-Z]+)*|^"([\001-\010\013\014\016-\037!#-\[\]-\177]|\\[\001-\011\013\014\016-\177])*")@((?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)$)|\[(25[0-5]|2[0-4]\d|[0-1]?\d?\d)(\.(25[0-5]|2[0-4]\d|[0-1]?\d?\d)){3}\]$/i.test(value);
+    },
+    "Sy&ouml;t&auml; toimiva s&auml;hk&ouml;postiosoite"
   );
 
   $('#crystallized').validate();
@@ -25,4 +26,9 @@ $(document).ready(function(){
       });
     });
   });
+
+  $('#errors a').on('click', function() {
+    $('#errors').slideUp();
+  });
+  
 });
