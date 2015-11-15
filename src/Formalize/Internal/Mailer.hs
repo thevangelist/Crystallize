6296156452b2fab17357d711af7a2b5bd0f31ce8
Matias Keveri <keveri@users.noreply.github.com>
2015-11-15 22:51:05 +0200

    Use correct MIME headers in email.

--- src/Formalize/Internal/Mailer.hs
+++ src/Formalize/Internal/Mailer.hs
@@ -8,8 +8,8 @@ import qualified Data.ByteString.Lazy as LB
 import           Data.Text            (Text)
 import qualified Data.Text            as T
 import qualified Data.Text.Lazy       as LT
-import           Network.Mail.Mime    hiding (simpleMail)
-import           Network.Mail.SMTP
+import           Network.Mail.Mime
+import           Network.Mail.SMTP (sendMailWithLogin')
 
 import           Formalize.Types
 
@@ -17,23 +17,16 @@ import           Formalize.Types
 emailPDF :: SMTPInfo -> Text -> PDF -> IO PDF
 emailPDF smtp toEmail pdf = do
     let subject = "Kiteyttäjä"
-        text    = plainTextPart emailMessage
         from    = Address Nothing (iSMTPFrom smtp)
-        to      = [Address Nothing toEmail]
-        file    = pdfPart pdf
-        mail    = simpleMail from to [] [] subject [file, text]
+        to      = Address Nothing toEmail
+        mail    = addPdf pdf $ simpleMail' to from subject emailMessage
     when (hostExists smtp) $ sendEmail smtp mail
     return pdf
 
--- Create PDF file attachment.
-pdfPart :: PDF -> Part
-pdfPart pdf =
-    Part
-        "application/pdf"
-        Base64
-        (Just "Kiteyttaja.pdf")
-        []
-        (LB.fromStrict pdf)
+-- Add PDF attachment to Mail.
+addPdf :: PDF -> Mail -> Mail
+addPdf = addAttachmentBS "application/pdf" "Kiteyttaja.pdf" . LB.fromStrict
+
 
 -- Send email using given STMP configuration.
 sendEmail :: SMTPInfo -> Mail -> IO ()
