69e7eacfb93276265ecfe3f75dca723ce02b6cfa
Esa Juhana Lahikainen <info@esajuhana.com>
2016-03-09 21:00:55 +0200

    Change of form contents for 2.0

--- web/static/custom.js
+++ web/static/custom.js
@@ -31,15 +31,4 @@ $(document).ready(function(){
       $('#errors').slideUp();
   });
 
-  var stepSlider = document.getElementById('slider-handles');
-
-  noUiSlider.create(stepSlider, {
-    start: [ 4000 ],
-    step: 1000,
-    range: {
-      'min': [  2000 ],
-      'max': [ 10000 ]
-    }
-  });
-
 });
